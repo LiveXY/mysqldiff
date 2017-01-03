467f8a6e286451f5dbd64ea99e850217b8e96f7a
xionghc <xionghc@happi-inc.com>
2017-01-03 14:50:07 +0800

    bug

--- Tools.js
+++ Tools.js
@@ -5,33 +5,33 @@ exports.exec = function (cmd, option) {
 		cp.exec(cmd, option, function(err, stdout, stderr) {
 			resolve(err ? err.message : stdout);
 		});
-    });
+	});
 }
 exports.dbConfig = function(config) {
 	var index1 = config.indexOf(':');
-    var index2 = config.lastIndexOf('@');
-    var index3 = config.lastIndexOf('~');
-    var index4 = config.lastIndexOf('#');
-    if (index1 == -1 || index2 == -1 || index3 == -1) return;
-    if (index4 == -1 || index4<index2) index4 = config.length;
+	var index2 = config.lastIndexOf('@');
+	var index3 = config.lastIndexOf('~');
+	var index4 = config.lastIndexOf('#');
+	if (index1 == -1 || index2 == -1 || index3 == -1) return;
+	if (index4 == -1 || index4<index2) index4 = config.length;
 
-    var user = config.substring(0, index1);
-    var password = config.substring(index1+1, index2);
-    var host = config.substring(index2+1, index3);
-    var database = config.substring(index3+1, index4);
-    var port = config.substring(index4+1);
+	var user = config.substring(0, index1);
+	var password = config.substring(index1+1, index2);
+	var host = config.substring(index2+1, index3);
+	var database = config.substring(index3+1, index4);
+	var port = config.substring(index4+1);
 
-    return {
-        'host': host,
-        'port': !port ? 3306 : parseInt(port),
-        'user': user,
-        'password': password,
-        'database': database
-    };
+	return {
+		'host': host,
+		'port': !port ? 3306 : parseInt(port),
+		'user': user,
+		'password': password,
+		'database': database
+	};
 }
 exports.rand = function(min, max) {
-    return min + Math.round(Math.random()*(max-min));
+	return min + Math.round(Math.random()*(max-min));
 }
 exports.randPort = function() {
-    return exports.rand(32000, 35000);
+	return exports.rand(32000, 35000);
 }
